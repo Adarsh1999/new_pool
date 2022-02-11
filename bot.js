f6b2c975472da9afd97312fc2eece8f200a9ba73
Adarsh Gupta <adarshguptamaurya@gmail.com>
2022-02-11 11:42:42 +0530

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,41 @@
+const ethers = require('ethers');
+
+
+const addresses = {
+  WETH: '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2',
+  factory: '0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f',
+
+  router: '0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D',
+  recipient: '0x04A206cf9fe9957EbAf776F7C4C9064074C93913'
+}
+
+const mnemonic = 'your mnemonic';
+
+
+const provider = new ethers.providers.WebSocketProvider('Infura websocket url to mainnet');
+const wallet = ethers.Wallet.fromMnemonic(mnemonic);
+const account = wallet.connect(provider);
+const factory = new ethers.Contract(
+  addresses.factory,
+  ['event PairCreated(address indexed token0, address indexed token1, address pair, uint)'],
+  account
+);
+const router = new ethers.Contract(
+  addresses.router,
+  [
+    'function getAmountsOut(uint amountIn, address[] memory path) public view returns (uint[] memory amounts)',
+    'function swapExactTokensForTokens(uint amountIn, uint amountOutMin, address[] calldata path, address to, uint deadline) external returns (uint[] memory amounts)'
+  ],
+  account
+);
+
+factory.on('PairCreated', async (token0, token1, pairAddress) => {
+  console.log(`
+    New pair detected
+    =================
+    token0: ${token0}
+    token1: ${token1}
+    pairAddress: ${pairAddress}
+  `);
+
+});
